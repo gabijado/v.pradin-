ef1cefada807cd7574951871eb40f336ccd93e8a
gabijado <gabija.dociute@gmail.com>
2025-09-10 21:03:21 +0300

    Update v0.1.cpp

--- v0.1.cpp
+++ v0.1.cpp
@@ -1,4 +1,4 @@
-#include <iosstream>
+#include <iostream>
 #include <iomanip>
 #include <vector>
 #include <string>
@@ -21,40 +21,24 @@ struct Studentas{
   int egz;
   double gal;
 };
-Studentas Stud_iv();
 
 int main(){
-  Vector<Studentas> Grupe;
-  cout<<"Kiek studentu grupeje? ";
-  int m;
-  cin >> m;
-  for (int z=0; z<m; z++)
-    Grupe.push_back(Stud_iv());
-  cout<<"Studento info: "<< endl;
-  cout<<left<<setw(15)<<"Vardas"<<"|"<<setw(20)<<"Pavarde"<<"|"<<setw(15)<<"Galutinis<<endl;
-  cout<<string(50, '-')<<endl;
-  for (auto Past:Grupe)
-{
-  cout << left << setw(15) << Past.var << "|" << setw(20) << Past.pav << "|" << setw(15) << fixed << setprecision(2) << Past.gal << endl;
-}
-}
-
-
-Studentas Stud_iv(){
- int n, laik_paz, sum=0;
-  cout << "Sveiki" << endl;
+  int n, laik_paz, sum=0;
   Studentas Pirmas;
   cout <<"Ivesk studento duomenis"<< endl;
   cout << "Vardas: "; cin>>Pirmas.var;
-  cout << "Pavardė: "; cin>>Pirmas.pav;
-  cout << "Kiek pažymių turi " << Pirmas.var<< " " << Pirmas.pav<<": "; cin >> n;
+  cout << "Pavarde: "; cin>>Pirmas.pav;
+  cout << "Kiek pažymiu turi " << Pirmas.var<< " " << Pirmas.pav<<": "; cin >> n;
   for (int a=0;a<n;a++)
   {
     cout<<a+1<<": "; cin >> laik_paz;
     Pirmas.paz.push_back(laik_paz);
     sum+=laik_paz; //sum+=Pirmas.paz[a];
   }
-  cout<<"Iveskite egzamino pazymi: "; cin>>Pirmas.egz;
+  cout<<"Iveskite egzamino pažymi: "; cin>>Pirmas.egz;
   Pirmas.gal = double(sum)/double(n)*0.4 + Pirmas.egz*0.6;
-  return Pirmas;
+  cout<<"Studento info: "<< endl;
+  cout<<left<<setw(15)<<"Vardas"<<"|"<<setw(20)<<"Pavarde"<<"|"<<setw(15)<<"Galutinis"<<endl; 
+  cout << string(50, '-') << endl;
+  cout << left << setw(15) << Pirmas.var << "|" << setw(20) << Pirmas.pav << "|"<<setw(15)<<fixed<<setprecision(2)<<Pirmas.gal << endl;
 }
