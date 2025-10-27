ec6c8875c8061bbdf4a17a7ffa2becf0eb683d4b
gabijado <gabija.dociute@gmail.com>
2025-10-27 12:44:06 +0200

    studentas.h
    Galutinis failo atnaujinimas.

--- studentas.h
+++ studentas.h
@@ -1,36 +1,16 @@
 #ifndef STUDENTAS_H
 #define STUDENTAS_H
 
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <list>
 #include <string>
-#include <algorithm>
-#include <limits>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <sstream>
-#include <chrono>
-
-using namespace std;
-using namespace std::chrono;
+#include <vector>
 
 struct Studentas {
-    string var;
-    string pav;
-    vector<int> paz;
+    std::string var;
+    std::string pav;
+    std::vector<int> paz;
     int egz{};
     double gal_vid{};
     double gal_med{};
 };
-
-double mediana(vector<int> paz);
-
-template <typename Container>
-Container skaitytiIsFailo(const string& failo_pav);
-
-void rodytiRezultatus(vector<Studentas>& studentai);
 
 #endif
