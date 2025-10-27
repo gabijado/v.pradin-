623c9db50fb532a292f58674e0ea1e2e05edcb2f
gabijado <gabija.dociute@gmail.com>
2025-10-27 12:45:58 +0200

    funkcijos.h
    Galutinis failo atnaujinimas.

---
+++ funkcijos.h
@@ -0,0 +1,34 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
+
+#include "studentas.h"
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+// Mediana
+double mediana(vector<int> paz);
+
+// Skaityti is failo (template deklaracija)
+template <typename Container>
+Container skaitytiIsFailo(const string& failo_pav);
+
+// Rodyti rezultatus
+void rodytiRezultatus(vector<Studentas>& studentai);
+
+// Skirstyti ir rasyti i failus
+template <typename Container>
+void skirstytiIrRasyti(const Container& studentai, const string& konteinerio_pav,
+    double& skirstymo_laikas, double& rasymo_laikas);
+
+// Generuoti testini faila
+void generuotiFaila(int kiekis);
+
+// Irasyti rezultatus i readme
+void irasytiRezultatusReadme(double v_read, double v_split, double v_write,
+    double l_read, double l_split, double l_write,
+    const string& failo_pav);
+
+#endif
