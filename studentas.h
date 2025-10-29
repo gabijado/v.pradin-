13634f5ffd2c80cccefdf24ddff93287a2df844d
gabijado <gabija.dociute@gmail.com>
2025-10-29 21:34:06 +0200

    studentas.h

---
+++ studentas.h
@@ -0,0 +1,16 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string var;
+    std::string pav;
+    std::vector<int> paz;
+    int egz{};
+    double gal_vid{};
+    double gal_med{};
+};
+
+#endif
