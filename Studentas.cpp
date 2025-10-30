78be2561db781b1e7a6c87f97256e94de0236ac2
gabijado <gabija.dociute@gmail.com>
2025-10-30 18:03:19 +0200

    studentas.cpp

--- Studentas.cpp
+++ Studentas.cpp
@@ -1,14 +1,9 @@
-#include "Studentas.h"
+#include "studentas.h"
 #include <algorithm>
 
 double mediana(std::vector<int> paz) {
     if (paz.empty()) return 0.0;
     std::sort(paz.begin(), paz.end());
     int n = paz.size();
-    if (n % 2 == 0) {
-        return (paz[n / 2 - 1] + paz[n / 2]) / 2.0;
-    }
-    else {
-        return paz[n / 2];
-    }
+    return (n % 2 == 0) ? (paz[n / 2 - 1] + paz[n / 2]) / 2.0 : paz[n / 2];
 }
